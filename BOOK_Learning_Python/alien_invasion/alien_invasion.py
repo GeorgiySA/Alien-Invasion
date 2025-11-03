03a031e45431f4a5f622aa04b590d384662077da
Georgiy Shvets <shvets_georgiy@mail.ru>
2025-11-03 15:22:11 +0300

    third commit

--- BOOK_Learning_Python/alien_invasion/alien_invasion.py
+++ BOOK_Learning_Python/alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -17,19 +18,25 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():  # Возвращ. список событий с момента последнего вызова функции.
-                if event.type == pygame.QUIT:
-                    sys.exit()  # Метод для выхода из игры.
+            self._check_events()
+            self._update_screen()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():  # Возвращ. список событий с момента последнего вызова функции.
+            if event.type == pygame.QUIT:
+                sys.exit()  # Метод для выхода из игры.
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+        pygame.display.flip()  # Отображение последнего прорисованного экрана.
 
 
 if __name__ == '__main__':
